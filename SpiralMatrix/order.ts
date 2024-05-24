53fbf4fff110032114e81091ab537ebd4382f958
Will Mitchell <wvmitchell@gmail.com>
2024-05-24 14:52:22 -0600

    solution for tests

--- SpiralMatrix/order.ts
+++ SpiralMatrix/order.ts
@@ -1,3 +1,23 @@
 export function spiralOrder(matrix: number[][]): number[] {
-  return []
+  let ans: number[] = [];
+
+  while(matrix.length > 0) {
+    ans = [...ans, ...matrix.shift()]
+
+    if(matrix.length > 0) {
+      let innerRows = matrix.slice(0, matrix.length - 1)
+
+      for(let row of innerRows) {
+        ans.push(row.pop())
+      }
+
+      ans = [...ans, ...matrix.pop().reverse()]
+
+      for(let row of innerRows.reverse()) {
+        ans.push(row.shift())
+      }
+    }
+  }
+
+  return ans
 }
