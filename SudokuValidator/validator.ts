b574ee522677be2ef1cd59c4e25c79349eb3030a
Will Mitchell <wvmitchell@gmail.com>
2024-05-24 14:27:27 -0600

    accepted solution

--- SudokuValidator/validator.ts
+++ SudokuValidator/validator.ts
@@ -1,2 +1,19 @@
 export function isValidSudoku(board: string[][]): boolean {
+  let rows = board
+  let cols = rows[0].map((_, i) => rows.map(row => row[i]))
+  let squares = []
+  rows.forEach((row, i) => row.forEach((_, j) => {
+    let squareIndex = 3 * Math.floor(i / 3) + Math.floor(j / 3)
+    squares[squareIndex] = squares[squareIndex] || []
+    squares[squareIndex].push(board[i][j])
+  }))
+
+  return rows.every(row => isValidGroup(row)) &&
+    cols.every(col => isValidGroup(col)) &&
+    squares.every(square => isValidGroup(square))
+}
+
+function isValidGroup(group: string[]): boolean {
+  let nums = group.filter(num => num !== '.')
+  return new Set(nums).size === nums.length
 }
