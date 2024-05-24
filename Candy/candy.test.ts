339dc6fd7a8ab6d2ed70336fb0c49977cb89aab6
Will Mitchell <wvmitchell@gmail.com>
2024-05-24 09:03:43 -0600

    working solution for simple tests

--- Candy/candy.test.ts
+++ Candy/candy.test.ts
@@ -5,31 +5,31 @@ describe('candy', () => {
     expect(candy([])).toBe(0)
   })
 
-  xit('should return 1 when ratings has only one element', () => {
+  it('should return 1 when ratings has only one element', () => {
     expect(candy([1])).toBe(1)
   })
 
-  xit('should return 2 when ratings has two elements with the same value', () => {
+  it('should return 2 when ratings has two elements with the same value', () => {
     expect(candy([1, 1])).toBe(2)
   })
 
-  xit('should return 3 when ratings has two elements with different values', () => {
+  it('should return 3 when ratings has two elements with different values', () => {
     expect(candy([2, 1])).toBe(3)
   })
 
-  xit('should return 3 when ratings has same two elements in reverse order', () => {
+  it('should return 3 when ratings has same two elements in reverse order', () => {
     expect(candy([1, 2])).toBe(3)
   })
 
-  xit('should return 4 when ratings has three elements with different values', () => {
+  it('should return 4 when ratings has three elements with different values', () => {
     expect(candy([1, 2, 1])).toBe(4)
   })
 
-  xit('should return 4 when ratings has three elements where the second and third elements have the same value', () => {
+  it('should return 4 when ratings has three elements where the second and third elements have the same value', () => {
     expect(candy([1, 2, 2])).toBe(4)
   })
 
-  xit('should return 5 when ratings has three elements where middle element is zero', () => {
+  it('should return 5 when ratings has three elements where middle element is zero', () => {
     expect(candy([1, 0, 2])).toBe(5)
   })
 })
--- Candy/candy.ts
+++ Candy/candy.ts
@@ -1,3 +1,24 @@
 export function candy(ratings: number[]): number {
-  return 0
+  // Strategy:
+  // 1. Create an array of the same length as ratings, initialized with 1. Since each child must have at least one candy.
+  // 2. Traverse the ratings array from left to right, and if the current rating is greater than the previous rating, increment the candy count for the current child.
+  // 3. Traverse the ratings array from right to left, and if the current rating is greater than the next rating, increment the candy count for the current child.
+  // 4. Return the sum of the candy array.
+
+  let candies = new Array(ratings.length).fill(1)
+  
+  for(let i = 0; i < ratings.length; i++) {
+    if(ratings[i] > ratings[i-1]) {
+      candies[i] = candies[i-1] + 1
+    }
+  }
+
+  for(let i = ratings.length - 1; i >= 0; i--) {
+    console.log('index: ', i)
+    if(ratings[i] > ratings[i+1]) {
+      candies[i] = candies[i+1] + 1
+    }
+  }
+
+  return candies.reduce((sum, num) => sum + num, 0)
 }
