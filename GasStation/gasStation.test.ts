bdd2b0a4261c224e905e936a26db0d9a168399ab
Will Mitchell <wvmitchell@gmail.com>
2024-08-18 10:14:36 -0400

    accepted solution

--- GasStation/gasStation.test.ts
+++ GasStation/gasStation.test.ts
@@ -12,4 +12,16 @@ describe('canCompleteCircuit', () => {
         const cost = [3, 4, 3];
         expect(canCompleteCircuit(gas, cost)).toBe(-1);
     })
+
+    it('should return 4', () => {
+        const gas = [5,1,2,3,4]
+        const cost = [4,4,1,5,1]
+        expect(canCompleteCircuit(gas, cost)).toBe(4)
+    })
+
+    it('should return -1', () => {
+        const gas = [4,5,2,6,5,3]
+        const cost = [3,2,7,3,2,9]
+        expect(canCompleteCircuit(gas, cost)).toBe(-1)
+    })
 })
--- GasStation/gasStation.ts
+++ GasStation/gasStation.ts
@@ -1,24 +1,23 @@
 function canCompleteCircuit(gas: number[], cost: number[]): number {
-    // this can be represented as a sliding window
-    // problem, with a variable length window
-    // start with two pointers, begin and end, both pointed at
-    // the first index
-    // as well as a variable for the tank, set to the amount at
-    // the first index
-    //
-    // while the end has not come back around to the beginning
-    // and the start has not looped back to the beginning
-    // if the tank has enough to get to the next station
-    // subtract the cost at the end pointer from the tank
-    // move the end pointer forward
-    // add the gas at the station to the tank
-    // otherwise (this path is no good)
-    // add to the tank the cost of moving from the start
-    // move the start forward (the window shrinks)
-    //
-    // if start index was in bounds, that's the answer
-    // otherwise, return -1
-    return -1
+    let start = 0
+    let tank = 0
+    let distance = 0
+
+    while (distance < gas.length && start < gas.length) {
+        let end = (start + distance) % gas.length
+        tank += gas[end]
+
+        if(tank >= cost[end]) {
+            tank -= cost[end]
+            distance += 1
+        } else {
+            tank = 0
+            start = start + distance + 1
+            distance = 0
+        }
+    }
+
+    return distance == gas.length ? start : -1
 }
 
 export { canCompleteCircuit }
